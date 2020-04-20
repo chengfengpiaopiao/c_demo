31b677ca692c668e20c89e97a44dbfa9f30e044b
chengfengpiaopiao <869010486@qq.com>
2020-04-20 17:42:11 +0800

    c++基本数据类型

--- another.cpp
+++ another.cpp
@@ -7,5 +7,28 @@ using std::string;
 
 void doNotion() {
 	string anotherStr = "hello i am from Another";
-	cout << anotherStr;
+	cout << anotherStr << endl;
+}
+
+void testVariable() {
+	int number = 100;
+	cout << ("int类型的变量" )  << number  << endl;
+
+	bool isNeedLearn = true;
+	cout << (isNeedLearn ? "需要" : "不需要") << "--测试bool" << endl;
+
+	char month[] = "字符串测试";
+	cout << month << endl;
+
+	string monthStr = "字符串测试2";
+	cout << monthStr << endl;
+
+	float quality = 0.0000000001;
+	cout << "小数测试float = " << quality << endl;
+
+	double qualityDouble = 0.0000001;
+	cout << "小数测试double = " << qualityDouble << endl;
+
+	wchar_t wChar[] = L"宽字符";
+	cout << wChar << endl;
 }
